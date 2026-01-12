73eaadeef6ba717e0f7f426b4bd89091b4b55f8e
calebbuilds <calebhoung@gmail.com>
2026-01-13 07:36:00 +1100

    pod install to build in xcode, succeeded

--- ios/App/App/AppDelegate.swift
+++ ios/App/App/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Capacitor
+import FirebaseCore 
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -8,6 +9,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+
+         // ✅ Initialize Firebase for native SDK
+        FirebaseApp.configure()
+        
         return true
     }
 
